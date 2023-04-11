d00ff314e009e08451295acbb1b3122ef6f287a0
Mufeng Tang <tangmufeng424@gmail.com>
2023-04-11 17:57:39 +0100

    easily switch between binary and cont data

--- mnist_comparison.py
+++ mnist_comparison.py
@@ -36,7 +36,7 @@ if not os.path.exists(model_path):
 
 # add parser as varaible of the main class
 parser = argparse.ArgumentParser(description='Sequential memories')
-parser.add_argument('--seq-len-pow', type=int, default=10, 
+parser.add_argument('--seq-len-max', type=int, default=10, 
                     help='max input length')
 parser.add_argument('--seed', type=int, default=[1], nargs='+',
                     help='seed for model init (default: 1); can be multiple, separated by space')
@@ -54,6 +54,8 @@ parser.add_argument('--query', type=str, default='online', choices=['online', 'o
                         offline means query with the predictions')
 parser.add_argument('--mode', type=str, default='train', choices=['train', 'recall'],
                     help='mode of the script: train or recall (just to save time)')
+parser.add_argument('--order', type=str, default='unorder', choices=['order', 'unorder'],
+                    help='whether to load digits following 0-9 order')
 parser.add_argument('--beta', type=int, default=1,
                     help='beta value for the MCHN')
 args = parser.parse_args()
@@ -84,7 +86,7 @@ def train_PC(pc, optimizer, seq, learn_iters, device):
     print(f'training PC complete, time: {time.time() - start_time}')
     return losses
 
-def _pc_recall(model, seq, query_type, device, binary=False):
+def _pc_recall(model, seq, device, args):
     """recall function for pc
     
     seq: PxN sequence
@@ -96,18 +98,18 @@ def _pc_recall(model, seq, query_type, device, binary=False):
     seq_len, N = seq.shape
     recall = torch.zeros((seq_len, N)).to(device)
     recall[0] = seq[0].clone().detach()
-    if query_type == 'online':
+    if args.query == 'online':
         # recall using true image at each step
-        recall[1:] = torch.sign(model(seq[:-1])) if binary else model(seq[:-1])
+        recall[1:] = torch.sign(model(seq[:-1])) if args.data_type == 'binary' else model(seq[:-1])
     else:
         # recall using predictions from previous step
         prev = seq[0].clone().detach() # 1xN
         for k in range(1, seq_len):
-            recall[k] = torch.sign(model(recall[k-1:k])) if binary else model(recall[k-1:k]) # 1xN
+            recall[k] = torch.sign(model(recall[k-1:k])) if args.data_type == 'binary' else model(recall[k-1:k]) # 1xN
 
     return recall
     
-def _hn_recall(model, seq, query_type, device, binary=False):
+def _hn_recall(model, seq, device, args):
     """recall function for pc
     
     seq: PxN sequence
@@ -119,15 +121,15 @@ def _hn_recall(model, seq, query_type, device, binary=False):
     seq_len, N = seq.shape
     recall = torch.zeros((seq_len, N)).to(device)
     recall[0] = seq[0].clone().detach()
-    if query_type == 'online':
+    if args.query == 'online':
         # recall using true image at each step
-        recall[1:] = torch.sign(model(seq, seq[:-1])) if binary else model(seq, seq[:-1]) # (P-1)xN
+        recall[1:] = torch.sign(model(seq, seq[:-1])) if args.data_type == 'binary' else model(seq, seq[:-1]) # (P-1)xN
     else:
         # recall using predictions from previous step
         prev = seq[0].clone().detach() # 1xN
         for k in range(1, seq_len):
             # prev = torch.sign(model(seq, prev)) if binary else model(seq, prev) # 1xN
-            recall[k] = torch.sign(model(seq, recall[k-1:k])) if binary else model(seq, recall[k-1:k]) # 1xN
+            recall[k] = torch.sign(model(seq, recall[k-1:k])) if args.data_type == 'binary' else model(seq, recall[k-1:k]) # 1xN
 
     return recall
 
@@ -139,20 +141,20 @@ def _plot_recalls(recall, model_name, args):
         ax[j].axis('off')
     plt.savefig(fig_path + f'/{model_name}_len{seq_len}_query{args.query}_data{args.data_type}', dpi=150)
 
-def _plot_memory(x, seed):
+def _plot_memory(x, seed, args):
     seq_len = x.shape[0]
     fig, ax = plt.subplots(1, seq_len, figsize=(seq_len, 1))
     for j in range(seq_len):
         ax[j].imshow(to_np(x[j].reshape(28, 28)), cmap='gray_r')
         ax[j].axis('off')
-    plt.savefig(fig_path + f'/memory_len{seq_len}_seed{seed}', dpi=150)
+    plt.savefig(fig_path + f'/memory_len{seq_len}_seed{seed}_data{args.data_type}', dpi=150)
 
-def _plot_PC_loss(loss, seq_len, learn_iters):
+def _plot_PC_loss(loss, seq_len, learn_iters, data_type='continuous'):
     # plotting loss for tunning; temporary
     plt.figure()
     plt.plot(loss, label='squared error sum')
     plt.legend()
-    plt.savefig(fig_path + f'/losses_len{seq_len}_iters{learn_iters}')
+    plt.savefig(fig_path + f'/losses_len{seq_len}_iters{learn_iters}_{data_type}')
 
 def main(args):
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -167,18 +169,19 @@ def main(args):
     learn_iters = args.epochs
     learn_lr = args.lr
     sep = args.HN_type
-    seq_len_pow = args.seq_len_pow
+    seq_len_max = args.seq_len_max
     query_type = args.query
     mode = args.mode
     beta = args.beta
     binary = True if args.data_type == 'binary' else False
+    order = True if args.order == 'order' else False
 
     # loop through different seq_len
     PC_MSEs = []
     HN_MSEs = []
-    for pow in np.arange(1, seq_len_pow):
 
-        seq_len = 2 ** pow
+    seq_lens = [2 ** pow for pow in range(1, seq_len_max)] if not order else range(2, seq_len_max)
+    for seq_len in seq_lens:
         if seq_len == 128:
             learn_iters += 100
         if seq_len == 256:
@@ -187,7 +190,7 @@ def main(args):
         print(f'Training variables: seq_len:{seq_len}; seed:{seed}')
 
         # load data
-        seq = load_sequence_mnist(seed, seq_len, order=False, binary=binary).to(device)
+        seq = load_sequence_mnist(seed, seq_len, order=order, binary=binary).to(device)
         seq = seq.reshape((seq_len, input_size)) # seq_lenx784
 
         # temporal PC
@@ -197,14 +200,14 @@ def main(args):
         # HN with linear separation function
         hn = ModernAsymmetricHopfieldNetwork(input_size, sep=sep, beta=beta).to(device)
 
-        PATH = os.path.join(model_path, f'PC_len{seq_len}_seed{seed}.pt')
+        PATH = os.path.join(model_path, f'PC_len{seq_len}_seed{seed}_{args.data_type}.pt')
         if mode == 'train':
             # training PC
             # note that there is no need to train MAHN - we can just write down the retrieval
             PC_losses = train_PC(pc, optimizer, seq, learn_iters, device)
             # save the PC model for later recall - because training PC is exhausting
             torch.save(pc.state_dict(), PATH)
-            _plot_PC_loss(PC_losses, seq_len, learn_iters)
+            _plot_PC_loss(PC_losses, seq_len, learn_iters, data_type=args.data_type)
 
         else:
             # recall mode, no training need, fast
@@ -212,8 +215,8 @@ def main(args):
             pc.eval()
 
             with torch.no_grad():
-                PC_recall = _pc_recall(pc, seq, query_type, device)
-                HN_recall = _hn_recall(hn, seq, query_type, device)
+                PC_recall = _pc_recall(pc, seq, device, args)
+                HN_recall = _hn_recall(hn, seq, device, args)
 
             if seq_len <= 16:
                 _plot_recalls(PC_recall, 'PC', args)
@@ -221,7 +224,7 @@ def main(args):
                 _plot_recalls(HN_recall, HN_name, args)
 
                 # plot the original memories
-                _plot_memory(seq, seed)
+                _plot_memory(seq, seed, args)
 
             # calculate MSE at each one, save file name with seed
             PC_MSEs.append(float(to_np(torch.mean((seq - PC_recall) ** 2))))
@@ -233,7 +236,7 @@ def main(args):
         results["PC"] = PC_MSEs
         results["HN"] = HN_MSEs
         print(results)
-        json.dump(results, open(num_path + f"/MSEs_seed{seed}_query{query_type}.json", 'w'))
+        json.dump(results, open(num_path + f"/MSEs_seed{seed}_query{query_type}_data{args.data_type}.json", 'w'))
 
 
 if __name__ == "__main__":
